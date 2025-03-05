cafd926c14a6f8435671f19774324ee80f38b046
Edgar Ruiz <edgarruiztovar@gmail.com>
2025-03-05 00:44:26 -0600

    Final version, just need to improve customize

--- Semestre1/Programacion-BD/Proyecto1/dashboard/server.R
+++ Semestre1/Programacion-BD/Proyecto1/dashboard/server.R
@@ -14,73 +14,98 @@ server <- function(input, output, session) {
   merge_assing_data <- read.csv(merge_assing_csv)
   merge2_data <- read.csv(merge2_csv)
   
-  # Datos agregados para gráficos de asignaciones e intercambios
-  summary_data_assign <- bubble_data %>%
-    group_by(size) %>%
-    summarise(avg_comparison_bubble = mean(comparison),
-              avg_swap_bubble = mean(swap)) %>%
-    inner_join(
-      merge_assing_data %>%
-        group_by(size) %>%
-        summarise(avg_comparison_merge = mean(comparison),
-                  avg_swap_merge = mean(swap)),
-      by = "size"
-    )
-  
-  summary_data_swaps <- bubble_data %>%
-    group_by(size) %>%
-    summarise(avg_swap_bubble = mean(swap)) %>%
-    inner_join(
-      merge2_data %>%
-        group_by(size) %>%
-        summarise(avg_swap_merge = mean(swap)),
-      by = "size"
-    )
-  
-  # Gráficos de la pestaña "Asignaciones"
-  output$plot_assignments <- renderPlotly({
-    p <- ggplot(summary_data_assign, aes(x = factor(size))) +
-      geom_bar(aes(y = avg_comparison_bubble, fill = "Bubble Sort"), stat = "identity", position = "dodge") +
-      geom_bar(aes(y = avg_comparison_merge, fill = "Merge Sort"), stat = "identity", position = "dodge") +
-      labs(y = "Promedio de Comparaciones", x = "Tamaño del Vector", fill = "Algoritmo") +
-      theme_minimal()
-    ggplotly(p)
-  })
-  
-  output$plot_assignments_line <- renderPlotly({
-    p <- ggplot(summary_data_assign, aes(x = size)) +
-      geom_line(aes(y = avg_comparison_bubble, color = "Bubble Sort")) +
-      geom_line(aes(y = avg_comparison_merge, color = "Merge Sort")) +
-      labs(y = "Promedio de Comparaciones", x = "Tamaño del Vector", color = "Algoritmo") +
-      theme_minimal()
-    ggplotly(p)
-  })
-  
-  # Gráficos de la pestaña "Intercambios"
-  output$plot_swaps <- renderPlotly({
-    p <- ggplot(summary_data_swaps, aes(x = factor(size))) +
-      geom_bar(aes(y = avg_swap_bubble, fill = "Bubble Sort"), stat = "identity", position = "dodge") +
-      geom_bar(aes(y = avg_swap_merge, fill = "Merge Sort"), stat = "identity", position = "dodge") +
-      labs(y = "Promedio de Intercambios", x = "Tamaño del Vector", fill = "Algoritmo") +
-      theme_minimal()
-    ggplotly(p)
-  })
-  
-  output$plot_swaps_line <- renderPlotly({
-    p <- ggplot(summary_data_swaps, aes(x = size)) +
-      geom_line(aes(y = avg_swap_bubble, color = "Bubble Sort")) +
-      geom_line(aes(y = avg_swap_merge, color = "Merge Sort")) +
-      labs(y = "Promedio de Intercambios", x = "Tamaño del Vector", color = "Algoritmo") +
-      theme_minimal()
-    ggplotly(p)
-  })
-  
   # Actualizar selectInput con tamaños disponibles
   observe({
     updateSelectInput(session, "size_select", choices = unique(bubble_data$size))
   })
   
-  # Gráfico de comparaciones con tres barras
+  # Gráficos de la pestaña "Asignaciones"
+  output$plot_assignments <- renderPlotly({
+    metric <- ifelse(input$assign_metric == "Comparison", "comparison", "swap")
+    summary_data <- bubble_data %>%
+      group_by(size) %>%
+      summarise(value_bubble = mean(.data[[metric]])) %>%
+      inner_join(
+        merge_assing_data %>%
+          group_by(size) %>%
+          summarise(value_merge = mean(.data[[metric]])),
+        by = "size"
+      )
+    
+    p <- ggplot(summary_data, aes(x = factor(size))) +
+      geom_bar(aes(y = value_bubble, fill = "Bubble Sort"), stat = "identity", position = "dodge") +
+      geom_bar(aes(y = value_merge, fill = "Merge Sort"), stat = "identity", position = "dodge") +
+      labs(y = paste("Promedio de", input$assign_metric), x = "Tamaño del Vector", fill = "Algoritmo") +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+  output$plot_assignments_line <- renderPlotly({
+    metric <- ifelse(input$assign_metric == "Comparison", "comparison", "swap")
+    summary_data <- bubble_data %>%
+      group_by(size) %>%
+      summarise(value_bubble = mean(.data[[metric]])) %>%
+      inner_join(
+        merge_assing_data %>%
+          group_by(size) %>%
+          summarise(value_merge = mean(.data[[metric]])),
+        by = "size"
+      )
+    
+    p <- ggplot(summary_data, aes(x = size)) +
+      geom_line(aes(y = value_bubble, color = "Bubble Sort")) +
+      geom_line(aes(y = value_merge, color = "Merge Sort")) +
+      labs(y = paste("Promedio de", input$assign_metric), x = "Tamaño del Vector", color = "Algoritmo") +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+  # Gráficos de la pestaña "Intercambios"
+  output$plot_swaps <- renderPlotly({
+    metric <- ifelse(input$swap_metric == "Comparison", "comparison", "swap")
+    summary_data <- bubble_data %>%
+      group_by(size) %>%
+      summarise(value_bubble = mean(.data[[metric]])) %>%
+      inner_join(
+        merge2_data %>%
+          group_by(size) %>%
+          summarise(value_merge = mean(.data[[metric]])),
+        by = "size"
+      )
+    
+    p <- ggplot(summary_data, aes(x = factor(size))) +
+      geom_bar(aes(y = value_bubble, fill = "Bubble Sort"), stat = "identity", position = "dodge") +
+      geom_bar(aes(y = value_merge, fill = "Merge Sort"), stat = "identity", position = "dodge") +
+      labs(y = paste("Promedio de", input$swap_metric), x = "Tamaño del Vector", fill = "Algoritmo") +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+  output$plot_swaps_line <- renderPlotly({
+    metric <- ifelse(input$swap_metric == "Comparison", "comparison", "swap")
+    summary_data <- bubble_data %>%
+      group_by(size) %>%
+      summarise(value_bubble = mean(.data[[metric]])) %>%
+      inner_join(
+        merge2_data %>%
+          group_by(size) %>%
+          summarise(value_merge = mean(.data[[metric]])),
+        by = "size"
+      )
+    
+    p <- ggplot(summary_data, aes(x = size)) +
+      geom_line(aes(y = value_bubble, color = "Bubble Sort")) +
+      geom_line(aes(y = value_merge, color = "Merge Sort")) +
+      labs(y = paste("Promedio de", input$swap_metric), x = "Tamaño del Vector", color = "Algoritmo") +
+      theme_minimal()
+    
+    ggplotly(p)
+  })
+  
+  # Gráficos de la pestaña "Comparaciones"
   output$plot_comparaciones <- renderPlotly({
     filtered_bubble <- bubble_data %>% filter(size == input$size_select)
     filtered_merge_assign <- merge_assing_data %>% filter(size == input$size_select)
@@ -98,7 +123,8 @@ server <- function(input, output, session) {
     p <- ggplot(plot_data, aes(x = Algoritmo, y = Comparaciones, fill = Algoritmo)) +
       geom_bar(stat = "identity", position = "dodge") +
       labs(y = "Número de Comparaciones", x = "Algoritmo") +
-      theme_minimal()
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
     
     ggplotly(p)
   })
@@ -121,7 +147,8 @@ server <- function(input, output, session) {
     p <- ggplot(plot_data, aes(x = Algoritmo, y = Intercambios, fill = Algoritmo)) +
       geom_bar(stat = "identity", position = "dodge") +
       labs(y = "Número de Intercambios", x = "Algoritmo") +
-      theme_minimal()
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
     
     ggplotly(p)
   })
